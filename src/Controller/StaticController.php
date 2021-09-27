dec4ad50a68e3ecb0cae2d60c83d1c27f45e0a15
Imrane <imranekent@gmail.com>
2021-09-27 10:57:35 +0200

    bg de valentin

--- src/Controller/StaticController.php
+++ src/Controller/StaticController.php
@@ -6,8 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\ContactType;
+use App\Form\InscriptionType;
 use App\Form\CommentaireType;
 use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Contact;
+use App\Entity\Commentaire;
+use App\Entity\Utilisateur;
 
 class StaticController extends AbstractController
 {
@@ -17,41 +21,91 @@ class StaticController extends AbstractController
         return $this->render('static/accueil.html.twig', []);
     }
 
-    #[Route('/contact', name: 'contact')]
-    public function contact(Request $request,\Swift_Mailer $mailer): Response
-    {
-        $form = $this->createForm(ContactType::class);
+    #[Route('/inscription', name: 'inscription')]
+    public function inscription(Request $request): Response
+    {   
+        $utilisateur = new Utilisateur();
+        $form = $this->createForm(InscriptionType::class,$utilisateur);
+
         if($request->isMethod('POST')){
             $form->handleRequest($request);
             if($form->isSubmitted() && $form->isValid()){
-                $nom = $form->get('nom')->getData();
-                $sujet = $form->get('sujet')->getData();
-                $contenu = $form->get('message')->getData();
+                $this->addFlash('notice','Inscription réussie');
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($utilisateur);
+                $em->flush();
+                return $this->redirectToRoute('inscription');
+            }
+        }
+        return $this->render('static/inscription.html.twig', ['form'=>$form->createView()]);
+    }
+
+    #[Route('/listeContact', name: 'listeContact')]
+    public function listeContact(): Response
+    {
+        $repoContact = $this->getDoctrine()->getRepository(Contact::class);
+        $contacts = $repoContact->findBy(array(),array('nom'=>'ASC'));
+        
+        return $this->render('static/listeContact.html.twig', ['contacts' => $contacts]);
+    }
+
+    #[Route('/listeCommentaire', name: 'listeCommentaire')]
+    public function listeCommentaire(): Response
+    {
+        $repoCommentaire = $this->getDoctrine()->getRepository(Commentaire::class);
+        $commentaires = $repoCommentaire->findBy(array(),array('nom'=>'ASC'));
+        
+        return $this->render('static/listeCommentaire.html.twig', ['commentaires' => $commentaires]);
+    }
+
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(Request $request,\Swift_Mailer $mailer): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class,$contact);
+        if($request->isMethod('POST')){
+            $form->handleRequest($request);
+            if($form->isSubmitted() && $form->isValid()){
+                
                 $this->addFlash('notice','Bouton appuyé');
-                $message = (new \Swift_Message($form->get('sujet')->getData()))
-                ->setFrom($form->get('email')->getData())
+                $message = (new \Swift_Message($contact->getSujet()))
+                ->setFrom($contact->getEmail())
                 ->setTo('imranekent@gmail.com')
                 //->setBody($form->get('message')->getData());
                 ->setBody($this->renderView('emails/contact-email.html.twig',array(
-                'nom'=>$nom,
-                'sujet'=>$sujet,
-                'contenu'=>$contenu)),'text/html');
+                'nom'=>$contact->getNom(),
+                'sujet'=>$contact->getSujet(),
+                'contenu'=>$contact->getMessage())),'text/html');
                 $mailer->send($message);
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($contact);
+                $em->flush();
+
                 return $this->redirectToRoute('contact');
             }
         }
         return $this->render('static/contact.html.twig', ['form' => $form -> createView()]);
     }
+    
 
     #[Route('/commentaire', name: 'commentaire')]
     public function commentaire(Request $request): Response
     {
-        $form = $this->createForm(CommentaireType::class);
+        $commentaire = new Commentaire();
+        $form = $this->createForm(CommentaireType::class,$commentaire);
         if($request->isMethod('POST')){
             $form->handleRequest($request);
             if($form->isSubmitted() && $form->isValid()){
-                $nom = $form->get('nom')->getData();
                 $this->addFlash('notice','Bouton appuyé ' . $nom);
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($commentaire);
+                $em->flush();
+
+                return $this->redirectToRoute('commentaire');
             }
         }
         return $this->render('static/commentaire.html.twig', ['form' => $form -> createView()]);
--- src/Form/InscriptionType.php
+++ src/Form/InscriptionType.php
@@ -5,23 +5,20 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class ContactType extends AbstractType
+
+class InscriptionType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom',TextType::class)
-            ->add('prenom',TextType::class)
-            ->add('rue',TextType::class)
-            ->add('codePostale',TextType::class)
-            ->add('ville',TextType::class)
-            ->add('tel',TextType::class)
-            ->add('envoyer',SubmitType::class)
+        ->add('nom',TextType::class)
+        ->add('prenom',TextType::class)
+        ->add('dateNaissance',BirthdayType::class)
+        ->add('envoyer',SubmitType::class)
         ;
     }
 
@@ -32,3 +29,4 @@ class ContactType extends AbstractType
         ]);
     }
 }
+
